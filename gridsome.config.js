ba0945aabc6a0b0fbbfb2ed7ce87714e531c35fa
zhanglin <1501103995@qq.com>
2021-08-05 01:16:44 +0800

    str

--- gridsome.config.js
+++ gridsome.config.js
@@ -13,6 +13,22 @@ module.exports = {
         typeName: 'BlogPost', //garpgQl query的对象名
         path: './content/blog/**/*.md',
       }
+    },
+    {
+      use: '@gridsome/source-strapi',
+      options: {
+        apiURL: 'http://localhost:1337',
+        queryLimit: 1000, // Defaults to 100
+        contentTypes: ['post'],
+        // singleTypes: ['impressum'], //单个节点
+        // Possibility to login with a Strapi user,
+        // when content types are not publicly available (optional).
+        //受保护的数据
+        // loginData: {
+        //   identifier: '',
+        //   password: ''
+        // }
+      }
     }
   ],
   transformers: {
